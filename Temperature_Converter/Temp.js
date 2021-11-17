c0133c4d6d7ec52bdeb41ca1c2ed0da96e67bb29
Abhishek Yadav <ay891241>
2021-11-17 20:29:08 +0530

    Temperature Converter

--- Temperature_Converter/Temp.js
+++ Temperature_Converter/Temp.js
@@ -1,7 +1,19 @@
 function convertTemp(){
     let Deg = document.getElementById('Degree').value;
- 
-    let Fah = (Deg*(9/5)+32);
 
-    document.getElementById('Fahrenheit').value = Fah+" f";
+    if(Deg!="")
+    {
+        let Fah = (Deg*(9/5)+32);
+
+      document.getElementById('Fahrenheit').value = Fah+" f";
+    }
+    else{
+        alert("Enter Temperature First !")
+    }
+    
+}
+
+function clearText(){
+    document.getElementById('Degree').value = "";
+    document.getElementById('Fahrenheit').value = "";
 }
