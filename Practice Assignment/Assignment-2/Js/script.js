b7915569f9e17240e5570988c3f63b3370f61f72
abhi616 <ay891241@gmail.com>
2022-04-10 22:09:35 +0530

    Assignment-2

---
+++ Practice Assignment/Assignment-2/Js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('Txt1').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+document.getElementById('Txt1').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+document.getElementById('Txt1').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
